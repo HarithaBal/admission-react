700fb379da7b2c82fa18ad6316a15df98c5aeb0e
vishnuchandrappan <cvishnu70@gmail.com>
2021-08-02 23:24:04 +0530

    feat(auth): add login persit

--- src/components/services/AuthService.js
+++ src/components/services/AuthService.js
@@ -1,31 +1,62 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { createContext } from "react";
 import jwt_decode from "jwt-decode";
+import { persistValue, getPersistedValue } from "../utils/helpers";
 
 export const AuthContext = createContext(null);
 
+const PERSIST_KEY = "auth";
+
+const defaultValue = {
+  isAuthenticated: false,
+  token: null,
+  user: null,
+};
+
 export const AuthService = ({ children }) => {
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-    const [token, setToken] = useState(null);
-    const [user, setUser] = useState(null);
+  const [state, setState] = useState(
+    getPersistedValue(PERSIST_KEY, defaultValue)
+  );
 
-    const authenticate = (token) => {
-        setIsAuthenticated(true);
-        setToken(token);
-        setUser(jwt_decode(token));
-    };
+  const updateState = (type, value) => {
+    setState((state) => {
+      const tempState = { ...state };
+      tempState[type] = value;
+      return tempState;
+    });
+  };
 
-    const logout = () => {
-        setIsAuthenticated(false);
-        setToken(null);
-        setUser(null);
-    };
+  const setIsAuthenticated = (value) => {
+    updateState("isAuthenticated", value);
+  };
 
-    return (
-        <AuthContext.Provider
-            value={{ isAuthenticated, user, token, authenticate, logout }}
-        >
-            {children}
-        </AuthContext.Provider>
-    );
+  const setToken = (value) => {
+    updateState("token", value);
+  };
+
+  const setUser = (value) => {
+    updateState("user", value);
+  };
+
+  const authenticate = (token) => {
+    setIsAuthenticated(true);
+    setToken(token);
+    setUser(jwt_decode(token));
+  };
+
+  const logout = () => {
+    setIsAuthenticated(false);
+    setToken(null);
+    setUser(null);
+  };
+
+  useEffect(() => {
+    persistValue(PERSIST_KEY, state);
+  }, [state]);
+
+  return (
+    <AuthContext.Provider value={{ ...state, authenticate, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
