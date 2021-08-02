c3ced50cc5cbafa589033851063ccbae583c7c6e
vishnuchandrappan <cvishnu70@gmail.com>
2021-08-02 17:03:38 +0530

    rebase from laravel project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { Routes } from "./components/Routes";
+import "antd/dist/antd.css";
+import "react-gravatar-or-initials/dist/index.css";
+import { DeclarationService } from "./components/services/DeclarationService";
+import { DataService } from "./components/services/DataService";
+import { AuthService } from "./components/services/AuthService";
+import { CommunityDataService } from "./components/services/CommunityDataService";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthService>
+      <BrowserRouter>
+        <CommunityDataService>
+          <DataService>
+            <DeclarationService>
+              <Routes />
+            </DeclarationService>
+          </DataService>
+        </CommunityDataService>
+      </BrowserRouter>
+    </AuthService>
   );
 }
 
