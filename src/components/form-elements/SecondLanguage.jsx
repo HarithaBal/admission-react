a8df0d1df9c8aee73232a4fe7e48ac8f2b3c9467
vishnuchandrappan <cvishnu70@gmail.com>
2021-08-25 21:49:28 +0530

    fix(students): change second lang to select

--- src/components/form-elements/SecondLanguage.jsx
+++ src/components/form-elements/SecondLanguage.jsx
@@ -1,20 +1,22 @@
-import React from "react"
-import {Input,Form} from "antd"
+import React from "react";
+import { Select, Form } from "antd";
 const SecondLanguage = () => {
-    return (
-            <Form.Item
-        rules={[
-          {
-            required: true,
-          },
-        ]}
-        label="Second language chosen"
-        name="second_language"
-      >
-        <Input placeholder="Eg: Hindi" />
-      </Form.Item>
-        
-    )
-}
+  return (
+    <Form.Item
+      rules={[
+        {
+          required: true,
+        },
+      ]}
+      label="Second language chosen"
+      name="second_language"
+    >
+      <Select>
+        <Select.Option value="malayalam">Malayalam</Select.Option>
+        <Select.Option value="hindi">Hindi</Select.Option>
+      </Select>
+    </Form.Item>
+  );
+};
 
 export default SecondLanguage;
