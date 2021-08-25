aa1b2b59cdf417e7542b9ca25424e788fd97dd1b
vishnuchandrappan <cvishnu70@gmail.com>
2021-08-25 22:21:06 +0530

    fix(student): alert if preferences are not chosen

--- src/components/forms/ManagementForm.jsx
+++ src/components/forms/ManagementForm.jsx
@@ -1,13 +1,13 @@
 import React, { useContext, useState } from "react";
 import {
-    Form,
-    Input,
-    Button,
-    DatePicker,
-    Radio,
-    Checkbox,
-    Divider,
-    message,
+  Form,
+  Input,
+  Button,
+  DatePicker,
+  Radio,
+  Checkbox,
+  Divider,
+  message,
 } from "antd";
 import { FormHeader } from "../FormHeader";
 import { Declaration } from "../Declaration";
@@ -32,234 +32,244 @@ import SecondLanguage from "../form-elements/SecondLanguage";
 const { Item } = Form;
 
 export const plainOptions = [
-    "Orthopaedical",
-    "Blind",
-    "Deaf",
-    "Mental/Brain Distress",
-    "Handicapped",
-    "Not Applicable",
+  "Orthopaedical",
+  "Blind",
+  "Deaf",
+  "Mental/Brain Distress",
+  "Handicapped",
+  "Not Applicable",
 ];
 
 export const ManagementForm = () => {
-    const [passing, setPassing] = useDate();
-    const [submitted, setSubmitted] = useState(false);
+  const [passing, setPassing] = useDate();
+  const [submitted, setSubmitted] = useState(false);
 
-    const { token } = useContext(AuthContext);
-    const { accepted, place } = useContext(DeclarationContext);
+  const { token } = useContext(AuthContext);
+  const { accepted, place } = useContext(DeclarationContext);
 
-    const {
-        name,
-        school,
-        gender,
-        dob,
-        guardian,
-        guardianOccupation,
-        state,
-        district,
-        taluk,
-        gramaPanchayath,
-        permanentAddress,
-        permanentPin,
-        isAddressSame,
-        currentAddress,
-        currentPin,
-        phone,
-        email,
-        havePreviousAttempts,
-        previousAttempts,
-        grades,
-        otherGrades,
-        preferences,
-    } = useContext(DataContext);
+  const {
+    name,
+    school,
+    gender,
+    dob,
+    guardian,
+    guardianOccupation,
+    state,
+    district,
+    taluk,
+    gramaPanchayath,
+    permanentAddress,
+    permanentPin,
+    isAddressSame,
+    currentAddress,
+    currentPin,
+    phone,
+    email,
+    havePreviousAttempts,
+    previousAttempts,
+    grades,
+    otherGrades,
+    preferences,
+  } = useContext(DataContext);
 
-    // check passing issue
-    // refactor differently abled
+  // check passing issue
+  // refactor differently abled
 
-    const onFinish = async (values) => {
-        const data = {
-            ...values,
-            school,
-            name,
-            gender,
-            dob,
-            guardian,
-            guardianOccupation,
-            state,
-            district,
-            taluk,
-            gramaPanchayath,
-            permanentAddress,
-            permanentPin,
-            phone,
-            email,
-            grades,
-            otherGrades,
-            preferences,
-            place,
-            passing,
-        };
+  const onFinish = async (values) => {
+    if (
+      !preferences.preference_1 ||
+      !preferences.preference_2 ||
+      !preferences.preference_3
+    ) {
+      alert("Please re-select your group and subject combinations.");
+    }
 
-        if (isAddressSame) {
-            data["currentAddress"] = data["permanentAddress"];
-            data["currentPin"] = data["permanentPin"];
-        } else {
-            data["currentAddress"] = currentAddress;
-            data["currentPin"] = currentPin;
-        }
-
-        data["previousAttempts"] = havePreviousAttempts ? previousAttempts : [];
-
-        try {
-            const response = await managementFormRequest(data, token);
-            message.success("Form submitted successfully");
-            setSubmitted(response.data);
-        } catch (e) {
-            message.error("Something went wrong. Please try again");
-        }
-
-        console.log("Received values of form: ", data);
+    const data = {
+      ...values,
+      school,
+      name,
+      gender,
+      dob,
+      guardian,
+      guardianOccupation,
+      state,
+      district,
+      taluk,
+      gramaPanchayath,
+      permanentAddress,
+      permanentPin,
+      phone,
+      email,
+      grades,
+      otherGrades,
+      preferences,
+      place,
+      passing,
     };
 
-    return (
-        <div className="form">
-            <FormHeader />
-            <div className="form--heading">
-                <h2>Management Quota</h2>
-                {submitted && (
-                    <h4>Application Number: 2021-22/M/{submitted?.data?.data?.id || "error"}</h4>
-                )}
-            </div>
-            <Divider />
-            <Form
-                name="management_form"
-                className="login-form"
-                initialValues={{
-                    remember: true,
-                }}
-                onFinish={onFinish}
-                labelCol={{ span: 24 }}
-                wrapperCol={{ span: 24 }}
-            >
-                <Item
-                    name="qualifying_examination"
-                    label="Name of Qualifying Examination"
-                    rules={[
-                        {
-                            required: true,
-                        },
-                    ]}
-                >
-                    <Input placeholder="Eg: SSLC" />
-                </Item>
-                <Item
-                    name="register_number"
-                    label="Register Number"
-                    rules={[
-                        {
-                            required: true,
-                        },
-                    ]}
-                >
-                    <Input type="number" placeholder="Register Number" />
-                </Item>
+    if (isAddressSame) {
+      data["currentAddress"] = data["permanentAddress"];
+      data["currentPin"] = data["permanentPin"];
+    } else {
+      data["currentAddress"] = currentAddress;
+      data["currentPin"] = currentPin;
+    }
 
-                <SchoolName />
+    data["previousAttempts"] = havePreviousAttempts ? previousAttempts : [];
 
-                <Item
-                    name="passing"
-                    label="Month & Year of passing"
-                    rules={[
-                        {
-                            required: true,
-                        },
-                    ]}
-                >
-                    <DatePicker
-                        onChange={setPassing}
-                        picker="month"
-                        placeholder="Month and year"
-                    />
-                </Item>
+    try {
+      const response = await managementFormRequest(data, token);
+      message.success("Form submitted successfully");
+      setSubmitted(response.data);
+    } catch (e) {
+      message.error("Something went wrong. Please try again");
+    }
 
-                <Name />
-                <Gender />
-                <DOB />
-                <Guardian />
+    console.log("Received values of form: ", data);
+  };
 
-                <Item
-                    name="religion"
-                    label="Religion"
-                    rules={[
-                        {
-                            required: true,
-                        },
-                    ]}
-                >
-                    <Input type="text" placeholder="Religion" />
-                </Item>
-                <Item
-                    name="caste"
-                    label="Caste"
-                    rules={[
-                        {
-                            required: true,
-                        },
-                    ]}
-                >
-                    <Input type="text" placeholder="Caste" />
-                </Item>
+  return (
+    <div className="form">
+      <FormHeader />
+      <div className="form--heading">
+        <h2>Management Quota</h2>
+        {submitted && (
+          <h4>
+            Application Number: 2021-22/M/{submitted?.data?.data?.id || "error"}
+          </h4>
+        )}
+      </div>
+      <Divider />
+      <Form
+        name="management_form"
+        className="login-form"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        labelCol={{ span: 24 }}
+        wrapperCol={{ span: 24 }}
+      >
+        <Item
+          name="qualifying_examination"
+          label="Name of Qualifying Examination"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Input placeholder="Eg: SSLC" />
+        </Item>
+        <Item
+          name="register_number"
+          label="Register Number"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Input type="number" placeholder="Register Number" />
+        </Item>
 
-                <Item
-                    name="is_latin_catholic"
-                    label="Whether applicant belongs to Latin Catholic (if yes, attach the copy of catechism certificate)"
-                    rules={[
-                        {
-                            required: true,
-                        },
-                    ]}
-                >
-                    <Radio.Group>
-                        <Radio value={false}>No</Radio>
-                        <Radio value={true}>Yes</Radio>
-                    </Radio.Group>
-                </Item>
+        <SchoolName />
 
-                <Item
-                    name="differently_abled"
-                    label="Whether applicant is differently abled (Choose relevant). If applicable, attach the medical certificate and supporting documents."
-                    rules={[
-                        {
-                            required: true,
-                        },
-                    ]}
-                >
-                    <Checkbox.Group options={plainOptions} />
-                </Item>
+        <Item
+          name="passing"
+          label="Month & Year of passing"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <DatePicker
+            onChange={setPassing}
+            picker="month"
+            placeholder="Month and year"
+          />
+        </Item>
 
-                <Location />
-                <Address />
-                <PhoneEmail />
-                <PreviousAttempts />
-                <Divider />
-                <Marks />
-                <Divider />
-                <Preferences />
-                <Divider />
-                <SecondLanguage />
-                <Divider />
-                <Declaration />
+        <Name />
+        <Gender />
+        <DOB />
+        <Guardian />
 
-                <Item>
-                    <Button
-                        type="primary"
-                        htmlType="submit"
-                        className="login-form-button"
-                        disabled={!accepted}
-                    >
-                        Submit Form
-                    </Button>
-                </Item>
-            </Form>
-        </div>
-    );
+        <Item
+          name="religion"
+          label="Religion"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Input type="text" placeholder="Religion" />
+        </Item>
+        <Item
+          name="caste"
+          label="Caste"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Input type="text" placeholder="Caste" />
+        </Item>
+
+        <Item
+          name="is_latin_catholic"
+          label="Whether applicant belongs to Latin Catholic (if yes, attach the copy of catechism certificate)"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Radio.Group>
+            <Radio value={false}>No</Radio>
+            <Radio value={true}>Yes</Radio>
+          </Radio.Group>
+        </Item>
+
+        <Item
+          name="differently_abled"
+          label="Whether applicant is differently abled (Choose relevant). If applicable, attach the medical certificate and supporting documents."
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Checkbox.Group options={plainOptions} />
+        </Item>
+
+        <Location />
+        <Address />
+        <PhoneEmail />
+        <PreviousAttempts />
+        <Divider />
+        <Marks />
+        <Divider />
+        <Preferences />
+        <Divider />
+        <SecondLanguage />
+        <Divider />
+        <Declaration />
+
+        <Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+            disabled={!accepted}
+          >
+            Submit Form
+          </Button>
+        </Item>
+      </Form>
+    </div>
+  );
 };
